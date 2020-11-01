a966554b84fba026ca378a9bdb4c9504397bd79a
FerhatUcar <ucarferhat3@gmail.com>
2020-11-01 22:25:35 +0100

    adds authors

--- server/app.ts
+++ server/app.ts
@@ -1,11 +1,10 @@
 import express = require("express");
 import { graphqlHTTP } from "express-graphql";
-import schema = require("./schema/schema");
+const schema = require("./schema/schema");
 
 const app = express();
 
-// @ts-ignore
-app.use("/graphql", graphqlHTTP({ schema }));
+app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
 
 app.listen(4000, () => {
   console.log("Running on port 4000");
--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,12 +1,24 @@
 import graphql = require("graphql");
 import * as _ from "lodash";
 
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
-const book = [
-  { name: "Name of the wind", genre: "Fantasy", id: "1" },
-  { name: "The final empire", genre: "Fantasy", id: "2" },
-  { name: "The long earth", genre: "Sci-Fi", id: "3" },
+const books = [
+  { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: "1" },
+  { name: "The final empire", genre: "Fantasy", id: "2", authorId: "2" },
+  { name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+];
+
+const authors = [
+  { name: "Patrick Roth", age: 44, id: "1" },
+  { name: "Brandon Sanderson", age: 42, id: "2" },
+  { name: "Terry Dick", age: 66, id: "3" },
 ];
 
 /**
@@ -17,9 +29,18 @@ const book = [
 const BookType = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+  }),
+});
+
+const AuthorType = new GraphQLObjectType({
+  name: "Author",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -28,9 +49,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     book: {
       type: BookType,
-      args: { id: { type: GraphQLString } },
+      args: { id: { type: GraphQLID } },
       resolve(parent, args) {
         // get data from db or other sources
+        return _.find(books, { id: args.id });
+      },
+    },
+    author: {
+      type: AuthorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(authors, { id: args.id });
       },
     },
   },
