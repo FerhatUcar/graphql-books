f652875fb29f99133807a517f3e4ff26c07faccc
FerhatUcar <ucarferhat3@gmail.com>
2020-11-02 18:37:09 +0100

    updates authorType with books and bookType with authors

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -7,12 +7,16 @@ const {
   GraphQLSchema,
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
 } = graphql;
 
 const books = [
   { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: "1" },
   { name: "The final empire", genre: "Fantasy", id: "2", authorId: "2" },
   { name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+  { name: "The hero of ages", genre: "Fantasy", id: "4", authorId: "2" },
+  { name: "The color of magic", genre: "Fantasy", id: "5", authorId: "3" },
+  { name: "The light fantastic", genre: "Fantasy", id: "6", authorId: "3" },
 ];
 
 const authors = [
@@ -26,21 +30,39 @@ const authors = [
  your objects within the whole schema to use it in the
  graphqlHTTP function
  */
-const BookType = new GraphQLObjectType({
+const BookType: any = new GraphQLObjectType({
   name: "Book",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+
+      // looking into the authors array to check if the
+      // parent.authorId matches the ID
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      },
+    },
   }),
 });
 
-const AuthorType = new GraphQLObjectType({
+const AuthorType: any = new GraphQLObjectType({
   name: "Author",
   fields: () => ({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    books: {
+      type: new GraphQLList(BookType),
+
+      // looking into the books array to check if the
+      // parent.id matches the authorId
+      resolve(parent, args) {
+        return _.filter(books, { authorId: parent.id });
+      },
+    },
   }),
 });
 
@@ -50,14 +72,20 @@ const RootQuery = new GraphQLObjectType({
     book: {
       type: BookType,
       args: { id: { type: GraphQLID } },
+
+      // find the ID of the book
       resolve(parent, args) {
         // get data from db or other sources
+        // looking into the books array and check
+        // if the args.id matches the book ID
         return _.find(books, { id: args.id });
       },
     },
     author: {
       type: AuthorType,
       args: { id: { type: GraphQLID } },
+
+      // find the ID of the author
       resolve(parent, args) {
         return _.find(authors, { id: args.id });
       },
