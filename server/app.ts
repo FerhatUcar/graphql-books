f5d43e1a7d5d56d7511d34e83b17b418d09d2447
FerhatUcar <ucarferhat3@gmail.com>
2020-11-15 12:48:43 +0100

    adds mongoose types and connects to db

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,22 @@
 import express = require("express");
 import { graphqlHTTP } from "express-graphql";
 const schema = require("./schema/schema");
+const MongoClient = require("mongodb").MongoClient;
+
+const uri =
+  "mongodb+srv://ferhat_31:deliall50@gql-ferhat.wnp3x.mongodb.net/gql-ferhat?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+
+client.connect((err: Error) => {
+  const collection = client.db("test").collection("devices");
+
+  console.log("Connected to database");
+
+  if (err) console.error(err.message);
+
+  // perform actions on the collection object
+  client.close();
+});
 
 const app = express();
 
