2394e41c451df5af23f527ceeb907f940765030f
FerhatUcar <ucarferhat3@gmail.com>
2020-11-18 18:45:47 +0100

    updates returns

--- server/schema/schema.ts
+++ server/schema/schema.ts
@@ -1,5 +1,4 @@
 import graphql = require("graphql");
-import * as _ from "lodash";
 
 const Book = require("../models/book");
 const Author = require("../models/author");
@@ -13,20 +12,20 @@ const {
   GraphQLList,
 } = graphql;
 
-const books = [
-  { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: "1" },
-  { name: "The final empire", genre: "Fantasy", id: "2", authorId: "2" },
-  { name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
-  { name: "The hero of ages", genre: "Fantasy", id: "4", authorId: "2" },
-  { name: "The color of magic", genre: "Fantasy", id: "5", authorId: "3" },
-  { name: "The light fantastic", genre: "Fantasy", id: "6", authorId: "3" },
-];
-
-const authors = [
-  { name: "Patrick Roth", age: 44, id: "1" },
-  { name: "Brandon Sanderson", age: 42, id: "2" },
-  { name: "Terry Dick", age: 66, id: "3" },
-];
+// const books = [
+//   { name: "Name of the wind", genre: "Fantasy", id: "1", authorId: "1" },
+//   { name: "The final empire", genre: "Fantasy", id: "2", authorId: "2" },
+//   { name: "The long earth", genre: "Sci-Fi", id: "3", authorId: "3" },
+//   { name: "The hero of ages", genre: "Fantasy", id: "4", authorId: "2" },
+//   { name: "The color of magic", genre: "Fantasy", id: "5", authorId: "3" },
+//   { name: "The light fantastic", genre: "Fantasy", id: "6", authorId: "3" },
+// ];
+//
+// const authors = [
+//   { name: "Patrick Roth", age: 44, id: "1" },
+//   { name: "Brandon Sanderson", age: 42, id: "2" },
+//   { name: "Terry Dick", age: 66, id: "3" },
+// ];
 
 /**
  Schema were you define Object types to connect
@@ -45,7 +44,7 @@ const BookType: any = new GraphQLObjectType({
       // looking into the authors array to check if the
       // parent.authorId matches the ID
       resolve(parent, args) {
-        // return _.find(authors, { id: parent.authorId });
+        return Author.findById(parent.authorId);
       },
     },
   }),
@@ -63,7 +62,7 @@ const AuthorType: any = new GraphQLObjectType({
       // looking into the books array to check if the
       // parent.id matches the authorId
       resolve(parent, args) {
-        // return _.filter(books, { authorId: parent.id });
+        return Book.find({ authorId: parent.id });
       },
     },
   }),
@@ -79,9 +78,9 @@ const RootQuery = new GraphQLObjectType({
       // find the ID of the book
       resolve(parent, args) {
         // get data from db or other sources
-        // looking into the books array and check
+        // looking into the books collection and check
         // if the args.id matches the book ID
-        // return _.find(books, { id: args.id });
+        return Book.findById(args.id);
       },
     },
     author: {
@@ -90,19 +89,19 @@ const RootQuery = new GraphQLObjectType({
 
       // find the ID of the author
       resolve(parent, args) {
-        // return _.find(authors, { id: args.id });
+        return Author.findById(args.id);
       },
     },
     books: {
       type: new GraphQLList(BookType),
       resolve(parent, args) {
-        // return books;
+        return Book.find({});
       },
     },
     authors: {
       type: new GraphQLList(AuthorType),
       resolve(parent, args) {
-        // return authors;
+        return Author.find({});
       },
     },
   },
