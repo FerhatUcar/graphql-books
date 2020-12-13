64886ad266a379404df6af38fa49bac993bcae47
FerhatUcar <ucarferhat3@gmail.com>
2020-12-13 22:00:14 +0100

    changes project react with to typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+import BookList from "./components/BookList";
+
+const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+})
+
+function App() {
+  return (
+      <ApolloProvider client={client}>
+          <div id="main">
+              <h1>Book app</h1>
+
+              <BookList />
+          </div>
+      </ApolloProvider>
+  );
+}
+
+export default App;
