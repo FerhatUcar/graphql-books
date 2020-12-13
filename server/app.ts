1e75b342104637ffeb401f9ef39c69030ff0995c
FerhatUcar <ucarferhat3@gmail.com>
2020-12-13 19:41:18 +0100

    adds react to project for client side

--- server/app.ts
+++ server/app.ts
@@ -13,7 +13,9 @@ const uri =
   "mongodb+srv://ferhat_31:deliall50@gql-ferhat.wnp3x.mongodb.net/books-db?retryWrites=true&w=majority";
 
 mongoose.connect(uri);
-mongoose.connection.once("open", () => console.log("Connected to database"));
+mongoose.connection.once("open", () =>
+  console.log("============> Connected to database!")
+);
 
 app.use("/graphql", graphqlHTTP({ schema, graphiql: true }));
-app.listen(4000, () => console.log("Running on port 4000"));
+app.listen(4000, () => console.log("============> Running on port 4000..."));
