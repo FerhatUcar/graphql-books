f5d02772a199a71ddf017fa627e238b81801cde0
FerhatUcar <ucarferhat3@gmail.com>
2020-12-14 20:57:15 +0100

    updates app component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 import ApolloClient from "apollo-boost";
-import {ApolloProvider} from "react-apollo";
-import BookList from "./components/BookList";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHookProvider } from "@apollo/react-hooks";
 
+// components
+import BookList from "./components/BookList";
+// import AddBook from './components/AddBook';
+
+// apollo client setup
 const client = new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
-})
+  uri: "http://localhost:4000/graphql",
+});
 
 function App() {
   return (
-      <ApolloProvider client={client}>
-          <div id="main">
-              <h1>Book app</h1>
-
-              <BookList />
-          </div>
-      </ApolloProvider>
+    <ApolloProvider client={client}>
+      <ApolloHookProvider client={client as any}>
+        <div id="main">
+          <h1>Book app</h1>
+          <BookList />
+        </div>
+      </ApolloHookProvider>
+    </ApolloProvider>
   );
 }
 
